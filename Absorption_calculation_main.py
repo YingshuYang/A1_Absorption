8738a3fe872371e5574be8b25a86109c7a510f7a
YingshuYang <yingshu001@e.ntu.edu.sg>
2024-03-07 10:31:38 +0800

    cleaned the code1

--- Absorption_calculation_main.py
+++ Absorption_calculation_main.py
@@ -29,39 +29,49 @@ from scipy.optimize import minimize
 
 
 ##########################################################################
-frequency       = 374.74e12    #THz
+
+'''Enter the frequency of the laser of which you are interested (units:Hz)''' 
+frequency       = 374.74e12    
 omega_single    = 2*pi*frequency
-f_omega_single  = 10   #the freq amp we set manully
+
+#the freq amp we set manully, this do not matter since we are finally going to calculte the percentages
+f_omega_single  = 10   
 
 
+''' Enter the dielectric constants for each layer for the frequency you entered above'''
+eps_Air         = cla2.epsilon(1,	0)                      # left most Air layer values n = 1, k = 0
+eps_1           = cla2.epsilon(3.8,	3.89)                   # Layer1 values n = x, k = y     
+eps_2           = cla2.epsilon(1.4608,	0.0013066)          # Layer2 values n = x, k = y    
+eps_3           = cla2.epsilon(1,	0)                      # Layer3 values n = x, k = y              
+eps_4           = cla2.epsilon(1,	0)                      # Layer4 values n = x, k = y                   
+eps_5           = cla2.epsilon(1,	0)                      # Layer5 values n = x, k = y 
+epsilon_optical = [eps_Air*8.85e-12,
+                   eps_1*8.85e-12,
+                   eps_2*8.85e-12,
+                   eps_3*8.85e-12,
+                   eps_4*8.85e-12,
+                   eps_5*8.85e-12]  #permittivity 
+mu              = 12.57e-7                                  #permeability set as constant
 
-eps_Air         = 1   
-eps_1           = cla2.epsilon(3.8,	3.89)             
-eps_2           = cla2.epsilon(1.4608,	0.0013066)    
-eps_3           = cla2.epsilon(1,	0)                  
-eps_4           = cla2.epsilon(1,	0)                     
-eps_5           = cla2.epsilon(1,	0)               #
-epsilon_optical = [eps_Air*8.85e-12,eps_1*8.85e-12,eps_2*8.85e-12,eps_3*8.85e-12,eps_4*8.85e-12,eps_5*8.85e-12]  #permittivity 
-mu              = 12.57e-7 
 
-
-
-d1min = 0.00001e-9; d1max = 100e-9#d1 = 100e-9
+'''Enter the thickness of each layer (units:m)'''
+d1 = 100e-9
 d2 = 0.3e-3
 d3 = 0
 d4 = 0
 d5 = 0
-number_of_thickness = 20
+
+
 
 
 
 ##########################################################################
 
 
-
+number_of_thickness = 1
 
 ZERO            = np.linspace(0,0,number_of_thickness)
-LAYER1          = np.linspace(d1min,d1max,number_of_thickness)
+LAYER1          = np.linspace(d1,d1,number_of_thickness)
 LAYER2          = np.linspace(d2,d2,number_of_thickness)       
 LAYER3          = np.linspace(d3,d3,number_of_thickness)         
 LAYER4          = np.linspace(d4,d4,number_of_thickness)
@@ -89,16 +99,16 @@ plt.plot(LAYER1,Absorption1,'--',label = 'absorption CoFeB')
 plt.ylim(0,1)
 plt.legend()
 
-print('Results')
+print('Results:')
 print('Absorption_layer1 = ', Absorption1)
 print('Absorption_layer2 = ', Absorption2)
 print('Absorption_layer3 = ', Absorption3)
 print('Absorption_layer4 = ', Absorption4)
 print('Absorption_layer5 = ', Absorption5)
 print('Absorption__total = ', Absorption_total)
-print('Transmission = ',transmission)
-print('Refelction = ',reflection)
-print('A+T+R = ',Absorption_total+transmission+reflection)
+print('Transmission      = ',transmission)
+print('Refelction        = ',reflection)
+print('A+T+R             = ',Absorption_total+transmission+reflection)
 
 
 
